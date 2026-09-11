590d4e6da6fa3bfca30d656a7d869bf5f6266f25
hindsahli <hindsahlii.2003@gmail.com>
2026-09-11 19:09:50 +0200

    jour 04 : terminer le niveau 1

---
+++ day04/exercices/exercice-04.js
@@ -0,0 +1,27 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 04 · EXERCICE 04 · NIVEAU 1 : FONDATIONS (DÉBUTANTS)
+ * PARCOURIR AVEC UN `FOR` CLASSIQUE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Créez nombres = [10, 20, 30, 40]. Utilisez une boucle for (avec i = 0 jusqu'à length) pour afficher
+ *  chaque nombre multiplié par 2.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-04
+ * ▶️ Commande : node day04/exercices/exercice-04.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+// TODO: écris ta solution ici.
+
+let nombres = [10, 20, 30, 40]
+ 
+let i = 0 
+while(i < nombres.length)
+{
+    console.log(nombres[i]*2)
+    i++
+}
